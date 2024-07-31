d21081b37bd1a8d80ddd93abf9ac19323d76d167
m1l0js <m1l0js@proton.me>
2024-07-31 17:13:52 +0200

    other

--- first.nim
+++ first.nim
@@ -1,12 +1,2 @@
-let x: int32 = 100
-case x:
-    of 5:
-        echo "five"
-    of 10:
-        echo "ten"
-
-    else:
-        echo "hacked"
-        discard
-
-echo "more testing"
+for n in 5 .. 9:
+    echo n
