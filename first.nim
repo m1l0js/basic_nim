a8b0b8b626f8d9bbbaacaafa8606139dc4dd6ec0
m1l0js <m1l0js@proton.me>
2024-07-31 17:41:59 +0200

    collatz_conjecture

--- first.nim
+++ first.nim
@@ -1,2 +1,13 @@
-for n in 5 .. 9:
-    echo n
+proc collatz(n: int) =
+  var num = n
+  while num != 1:
+    echo num
+    if num mod 2 == 0:
+      num = num div 2
+    else:
+      num = 3 * num + 1
+  echo num  # Print the final 1
+
+# Example usage
+let startNum = 5
+collatz(startNum)
